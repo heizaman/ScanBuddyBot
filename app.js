17edb4e5a2cb1a660da60d52b14c043e8de331a1
Aman <msgmeaman@gmail.com>
2017-10-29 02:40:50 +0530

    App Id for deployed Bot

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: /*process.env.MICROSOFT_APP_ID*/'7228fde0-293d-4224-8e14-c7a71460fefe',
+    appPassword: /*process.env.MICROSOFT_APP_PASSWORD*/'tquXWZ61!>$uxhtBUFZ921>'
 });
 
 
